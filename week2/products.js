439f6ce97d240233f7bba79c678049708160d7f6
Yuli <145248933+UserYuli@users.noreply.github.com>
2024-01-15 23:57:39 +0800

    Add files via upload
    week2作業上傳

---
+++ week2/products.js
@@ -0,0 +1,46 @@
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+
+createApp({
+  data() {
+    return {
+      apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+      apiPath: 'viosa-vue',
+      products: [],
+      tempProduct: {},
+    }
+  },
+  methods: {
+    checkAdmin() {
+      const url = `${this.apiUrl}/api/user/check`;
+      axios.post(url)
+        .then(() => {
+          this.getProducts();
+        })
+        .catch((err) => {
+          alert(err.response.data.message)
+          window.location = 'login.html';
+        })
+    },
+    getProducts() {
+      const url = `${this.apiUrl}/api/${this.apiPath}/admin/products`;
+      axios.get(url)
+        .then((response) => {
+          this.products = response.data.products;
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        })
+    },
+    openProduct(item) {
+      this.tempProduct = item;
+    }
+  },
+  mounted() {
+    // 取出 Token
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+    // 把Token 帶到headers的Authorization裡
+    axios.defaults.headers.common.Authorization = token;
+
+    this.checkAdmin()
+  }
+}).mount('#app');
